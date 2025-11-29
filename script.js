69a85395148ac196c7e309b4988374bfce574691
arkey <arkey@ncsu.edu>
2025-11-29 12:39:44 -0500

    Removed hamburger menu & replaced w/ back arrow to home, removed unused html files

--- script.js
+++ script.js
@@ -35,26 +35,6 @@ window.addEventListener('load', matchImageHeight); // Run on page load
 window.addEventListener('resize', matchImageHeight); // Run on window resize
 
 /* ----------------------------
-   MOBILE HAMBURGER MENU
------------------------------ */
-const hamburger = document.getElementById("hamburger");
-const menu = document.getElementById("mobile-menu");
-const overlay = document.getElementById("menu-overlay");
-
-hamburger.addEventListener("click", () => {
-    hamburger.classList.toggle("active");
-    menu.classList.toggle("active");
-    overlay.classList.toggle("active");
-});
-
-// Clicking outside closes menu
-overlay.addEventListener("click", () => {
-    hamburger.classList.remove("active");
-    menu.classList.remove("active");
-    overlay.classList.remove("active");
-});
-
-/* ----------------------------
    IMAGE CAROUSELS (PROJECT PAGES)
    This automatically finds ALL carousels on the site.
 ----------------------------- */
